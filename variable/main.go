4b0a63b0b4dee4b556bcd9290fa86cb31e4d5ed4
hydraveer <veerendrapatel420@gmail.com>
2025-01-07 14:34:57 +0530

    Userinput

---
+++ variable/main.go
@@ -0,0 +1,22 @@
+package main
+
+
+const LoginToken string = "akjdfhjkadsf" // because L is capital this mean it can now public and access by this program anywhere
+func main() {
+	// var username string = "Veer"
+	// var isLogged bool = true
+	// fmt.Println(isLogged)
+	// fmt.Printf("Variable type : %T \n", isLogged)
+	// fmt.Println(username)
+	// fmt.Printf("Variable type : %T \n", username)
+	// var x int;
+	// fmt.Println(x);
+	// fmt.Printf("The type of this %T \n", x);
+	//without datatype
+	// var name = "veer";
+	// fmt.Println(name)
+	//no var style
+	// numberOfUser := 20;
+	// fmt.Println(numberOfUser)
+	// fmt.Println(LoginToken)
+}
